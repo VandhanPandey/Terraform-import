7848a55274b38a83779ad4256d45202405f91c45
VandhanPandey <vandhanpandey995@gmail.com>
2026-04-23 02:06:57 +0530

    Actions S3 Bucket

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,10 +1,10 @@
-terraform{
-    backend "s3"{
-        bucket="vandhan-tf-state"
-        key="dev/terraform.tfstate"
-        region="us-east-1"
-        encrypt=true
-        use_lockfile=true
-        
-    }
+terraform {
+  backend "s3" {
+    bucket       = "vandhan-tf-state"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+
+  }
 }
--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "tf_bucket" {
-  bucket = "import-test-vandhan"
+  bucket = "export-test-vandhan"
 }
