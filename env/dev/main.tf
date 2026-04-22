695e578082273a764dd7011773f0080ea78c18fc
VandhanPandey <vandhanpandey995@gmail.com>
2026-04-23 01:56:00 +0530

    S3 Created, EC2 deleted

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "myec2"{
-    ami = "ami-076d128fb049922d4"
-    instance_type = "t3.micro"
-    tags = {
-        Name= "Sample EC2"
-    }
-}
-
 resource "aws_s3_bucket" "tf_bucket" {
   bucket = "import-test-vandhan"
 }
